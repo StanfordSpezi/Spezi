6387b4027d6b7d2b67995f2d6dbc44d0c4f7decf
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-12-08 11:22:59 -0800

    Increase Timeout

--- Tests/UITests/TestAppUITests/Helper/XCTTestCase+HealthDataType.swift
+++ Tests/UITests/TestAppUITests/Helper/XCTTestCase+HealthDataType.swift
@@ -154,7 +154,7 @@ extension XCTestCase {
             healthApp.staticTexts["Continue"].tap()
             XCTAssertTrue(healthApp.tables.buttons["Next"].waitForExistence(timeout: 2.0))
             healthApp.tables.buttons["Next"].tap()
-            XCTAssertTrue(healthApp.staticTexts["Continue"].waitForExistence(timeout: 2.0))
+            XCTAssertTrue(healthApp.staticTexts["Continue"].waitForExistence(timeout: 20.0))
             healthApp.staticTexts["Continue"].tap()
         }
         
