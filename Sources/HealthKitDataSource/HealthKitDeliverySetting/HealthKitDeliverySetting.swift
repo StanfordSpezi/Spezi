b9eab07dfb5ddfe4a50c7ba1bb782e2e7f3f51f2
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-12-08 16:33:58 -0800

    Add HealthKit Documentation

--- Sources/HealthKitDataSource/HealthKitDeliverySetting/HealthKitDeliverySetting.swift
+++ Sources/HealthKitDataSource/HealthKitDeliverySetting/HealthKitDeliverySetting.swift
@@ -7,24 +7,26 @@
 //
 
 
-/// <#Description#>
+/// Determines the data delivery settings for any ``HealthKitDataSource`` used in the HealthKit component.
 public enum HealthKitDeliverySetting: Equatable {
-    /// <#Description#>
+    /// The HealthKit data is manually collected when the ``HealthKit/triggerDataSourceCollection()`` function is called.
     case manual(safeAnchor: Bool = true)
-    /// <#Description#>
-    case anchorQuery(HealthKitDeliveryStartSetting = .afterAuthorizationAndApplicationWillLaunch, safeAnchor: Bool = true)
-    /// <#Description#>
-    case background(HealthKitDeliveryStartSetting = .afterAuthorizationAndApplicationWillLaunch, safeAnchor: Bool = true)
+    /// The HealthKit data is collected based on the `HealthKitDeliveryStartSetting` and constantly listens to updates while the application is running.
+    /// If `safeAnchor` is enabled the `HKQueryAnchor` is persisted across multiple application launches using the user defaults.
+    case anchorQuery(HealthKitDeliveryStartSetting = .afterAuthorizationAndApplicationWillLaunch, saveAnchor: Bool = true)
+    /// The HealthKit data is collected based on the `HealthKitDeliveryStartSetting` and constantly listens to updates even if the application is not running.
+    /// If `safeAnchor` is enabled the `HKQueryAnchor` is persisted across multiple application launches using the user defaults.
+    case background(HealthKitDeliveryStartSetting = .afterAuthorizationAndApplicationWillLaunch, saveAnchor: Bool = true)
     
     
     var saveAnchor: Bool {
         switch self {
-        case let .manual(safeAnchor):
-            return safeAnchor
-        case let .anchorQuery(_, safeAnchor):
-            return safeAnchor
-        case let .background(_, safeAnchor):
-            return safeAnchor
+        case let .manual(saveAnchor):
+            return saveAnchor
+        case let .anchorQuery(_, saveAnchor):
+            return saveAnchor
+        case let .background(_, saveAnchor):
+            return saveAnchor
         }
     }
     
