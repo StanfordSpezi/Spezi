851e26fb0bc4732e2510f378fe1265b19c92dc31
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-02-15 18:40:24 -0800

    Improve REUSE conformance

--- Sources/FHIRMockDataStorageProvider/MockUploadList.swift
+++ Sources/FHIRMockDataStorageProvider/MockUploadList.swift
@@ -1,5 +1,5 @@
 //
-// This source file is part of the Stanford CardinalKit Template Application project
+// This source file is part of the CardinalKit open-source project
 //
 // SPDX-FileCopyrightText: 2023 Stanford University
 //
