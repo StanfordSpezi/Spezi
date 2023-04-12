b5476acaeb57092068a945e7750ab84ce02ddbc6
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-04-12 10:05:35 -0700

    Support Swift 5.7 (#60)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.7
 
 //
 // This source file is part of the CardinalKit open-source project
@@ -22,7 +22,7 @@ let package = Package(
         .library(name: "XCTCardinalKit", targets: ["XCTCardinalKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/StanfordBDHG/XCTRuntimeAssertions", .upToNextMinor(from: "0.2.0"))
+        .package(url: "https://github.com/StanfordBDHG/XCTRuntimeAssertions", .upToNextMinor(from: "0.2.1"))
     ],
     targets: [
         .target(
