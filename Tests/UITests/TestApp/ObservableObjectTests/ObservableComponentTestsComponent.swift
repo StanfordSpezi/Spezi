60c7576ba6bff8db217214b81cd78c5abc334838
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-05-11 20:03:44 -0700

    Update to Spezi

--- Tests/UITests/TestApp/ObservableObjectTests/ObservableComponentTestsComponent.swift
+++ Tests/UITests/TestApp/ObservableObjectTests/ObservableComponentTestsComponent.swift
@@ -1,12 +1,12 @@
 //
-// This source file is part of the CardinalKit open-source project
+// This source file is part of the Spezi open-source project
 //
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
 
-import CardinalKit
+import Spezi
 import SwiftUI
 
 
--- Tests/UITests/TestApp/ObservableObjectTests/ObservableObjectTests.swift
+++ Tests/UITests/TestApp/ObservableObjectTests/ObservableObjectTests.swift
@@ -1,13 +1,13 @@
 //
-// This source file is part of the CardinalKit open-source project
+// This source file is part of the Spezi open-source project
 //
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
 
-import CardinalKit
-import XCTCardinalKit
+import Spezi
+import XCTSpezi
 import XCTestApp
 
 
