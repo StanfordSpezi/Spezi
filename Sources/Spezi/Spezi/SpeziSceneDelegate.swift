b45a2fe26b24af4e201479c29fcdc7c6e338b28a
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-06-27 02:11:26 -0700

    Separate Scene Delegate in separate class to avoid double initialization

---
+++ Sources/Spezi/Spezi/SpeziSceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+
+class SpeziSceneDelegate: NSObject, UISceneDelegate {
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        guard let delegate = SpeziAppDelegate.appDelegate else {
+            return
+        }
+        delegate.sceneWillEnterForeground(scene)
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        guard let delegate = SpeziAppDelegate.appDelegate else {
+            return
+        }
+        delegate.sceneDidBecomeActive(scene)
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        guard let delegate = SpeziAppDelegate.appDelegate else {
+            return
+        }
+        delegate.sceneWillResignActive(scene)
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        guard let delegate = SpeziAppDelegate.appDelegate else {
+            return
+        }
+        delegate.sceneDidEnterBackground(scene)
+    }
+}
