d6986157b3d4a2a0379c4116226447e6ae2b90a8
Andreas Bauer <andreas.bauer@stanford.edu>
2024-03-08 08:09:10 -0800

    Docs

--- Sources/Spezi/Module/ModuleCollection.swift
+++ Sources/Spezi/Module/ModuleCollection.swift
@@ -11,6 +11,7 @@
 ///
 /// You can not create a ``ModuleCollection`` yourself. Please use the ``ModuleBuilder`` to create a ``ModuleCollection``.
 public class ModuleCollection {
+    /// The elements of the collection.
     @_spi(Spezi)
     public let elements: [any Module]
 
