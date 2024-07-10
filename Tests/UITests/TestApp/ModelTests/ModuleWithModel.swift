75ec9dc993065e355cad22ef1ee16d37e99e9e95
Andreas Bauer <andreas.bauer@stanford.edu>
2024-07-10 12:20:05 +0200

    Lint

--- Tests/UITests/TestApp/ModelTests/ModuleWithModel.swift
+++ Tests/UITests/TestApp/ModelTests/ModuleWithModel.swift
@@ -38,7 +38,7 @@ private struct MyModifier: ViewModifier {
 
 
 class ModuleWithModel: Module, EnvironmentAccessible {
-    @Application(\.launchOptions) private var launchOptions // TODO: how to ensure main actor isolation?
+    @Application(\.launchOptions) private var launchOptions
 
     @Model var model = MyModel2(message: "Hello World")
 
