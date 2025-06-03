dcabec96b78a394e08b3858e6893d594045b37d7
Andreas Bauer <andreas.bauer@stanford.edu>
2025-06-03 22:22:05 +0200

    Header

--- Sources/Spezi/Spezi/ServiceModuleGroup.swift
+++ Sources/Spezi/Spezi/ServiceModuleGroup.swift
@@ -1,5 +1,14 @@
+//
+// This source file is part of the Stanford XCTestExtensions open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import OSLog
 import SpeziFoundation
+
 
 struct ServiceModuleGroup {
     private enum Input {
