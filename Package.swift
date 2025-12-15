7e933a854070a9727040c0cd4161215bd811368c
Philipp Nagy <philipp.nagy@tum.de>
2025-12-15 20:08:26 +0100

    Remove swift-log dependency

--- Package.swift
+++ Package.swift
@@ -31,8 +31,7 @@ let package = Package(
         .package(url: "https://github.com/StanfordSpezi/SpeziFoundation.git", from: "2.5.4"),
         .package(url: "https://github.com/StanfordBDHG/XCTRuntimeAssertions.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.1"),
-        .package(url: "https://github.com/dfed/swift-testing-expectation", .upToNextMinor(from: "0.1.4")),
-        .package(url: "https://github.com/apple/swift-log", from: "1.6.0")
+        .package(url: "https://github.com/dfed/swift-testing-expectation", .upToNextMinor(from: "0.1.4"))
     ] + swiftLintPackage(),
     targets: [
         .target(
